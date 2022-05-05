a8e819b17c4b6d60c3e23ca611ca5c216094154d
cesar jesus leon martin <cesilius98@gmail.com>
2022-05-05 11:45:33 +0200

    Feature: creacion del main y de la implementacion del codigo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
+import com.company.dataestructure.Stack;
 
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        Stack pila=new Stack();
+        System.out.println("introduce el primer numero");
+        pila.push(sc.nextInt());
+        sc.nextLine();
+        System.out.println("introduce el segundo numero");
+        pila.push(sc.nextInt());
+        sc.nextLine();
+        System.out.println("introduce un operador");
+        String operador=sc.nextLine();
+        switch (operador){
+            case "+":
+                int numero= pila.pop();
+                numero=numero+ pila.pop();
+                System.out.println(numero);
+                break;
+            case "-":
+                numero= pila.pop();
+                numero=numero - pila.pop();
+                System.out.println(numero);
+                break;
+            case "*":
+                numero= pila.pop();
+                numero=numero * pila.pop();
+                System.out.println(numero);
+                break;
+            case "/":
+                numero= pila.pop();
+                numero=numero / pila.pop();
+                System.out.println(numero);
+                break;
+            default:
+                System.out.println("operador erroeneo");
+        }
     }
 }
--- src/com/company/dataestructure/Stack.java
+++ src/com/company/dataestructure/Stack.java
@@ -1,8 +1,6 @@
 package com.company.dataestructure;
 
-import java.util.Collection;
-
-public class GenericStack <T> {
+public class Stack{
     private static final int CAPACIDAD_INICIAL =5;
     private static final int TOP = 0;
     private CustomCollection  collection = new CustomCollection(CAPACIDAD_INICIAL);
